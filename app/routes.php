205aae97e5217e42a5b13141a778c2c4e1e0706f
Philo van Kemenade <philovankemenade@gmail.com>
2016-03-31 20:32:34 +0200

    validate user tags input is human using recaptcha

--- app/routes.php
+++ app/routes.php
@@ -168,10 +168,21 @@ Route::post('dielo/{id}/addTags', function($id)
 	$item = Item::find($id);
 	$newTags = Input::get('tags');
 
-	foreach ($newTags as $newTag) {
-		$item->tag($newTag);
+	// validate that user is human with recaptcha
+	$secret = '6LeGGxwTAAAAAEu6-glsHHUFgEJS7Pk3BT3BITTs';
+	$recaptcha = new \ReCaptcha\ReCaptcha($secret);
+	$resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
+	if ($resp->isSuccess()) {
+	    // add new tags
+	    foreach ($newTags as $newTag) {
+			$item->tag($newTag);
+		}
+	} else {
+		// validation unsuccessful
+		// TODO: error message?
+	    return Redirect::to($item->getUrl());
 	}
-
+	
 	return Redirect::to($item->getUrl());
 });
 
